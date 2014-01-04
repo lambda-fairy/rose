4683d01f244ffc500788f9065eb677185d931982
Chris Wong <lambda.fairy@gmail.com>
2014-01-04 14:07:54 +1300

    Remove broken VM implementation

--- main.rs
+++ main.rs
@@ -1,22 +1,8 @@
 #[allow(dead_code)];
 #[feature(macro_rules)];
 
-//mod vm;
 mod charclass;
 mod parse;
-
-/*
-fn main() {
-    let prog = [
-        vm::Fork(1, 3),
-        vm::Range('a' as u8, 'a' as u8),
-        vm::Jump(3),
-        vm::Range('b' as u8, 'b' as u8),
-        vm::Done,
-        ];
-    println!("{:?}", vm::recursive(prog, "b"));
-}
-*/
 
 fn main() {
     println!("{:?}", parse::parse("(?:a|b){,234}?c"))
