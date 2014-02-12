38701bfe112c625f478e9a9d53abd2aed33777d8
Chris Wong <lambda.fairy@gmail.com>
2014-02-12 19:59:56 +1300

    Compile bounded repetitions

--- src/compile.rs
+++ src/compile.rs
@@ -1,22 +1,32 @@
 //! State machine compiler.
 
+use std::mem::replace;
+
 use parse;
-use parse::{Expr, Greedy, NonGreedy};
+use parse::{Expr, NonGreedy};
 use vm::{Program, State, Want, Range};
 
 
 /// Compile an AST into a `Program`.
 pub fn compile(e: &Expr) -> Program {
     let mut b = Builder::new();
-    let tails = compile_expr(&mut b, &[Initial], e);
+    let tails = compile_expr(&mut b, &[(Initial, Low)], e);
     b.reify(tails)
 }
 
 
+type Position = (Pos, Prec);
+
 #[deriving(Clone)]
-enum Position {
+enum Pos {
     Initial,
     Index(uint)
+}
+
+#[deriving(Clone)]
+enum Prec {
+    Low,
+    High
 }
 
 
@@ -36,18 +46,22 @@ impl Builder {
     fn push(&mut self, w: Want) -> ~[Position] {
         let tail = self.states.len();
         self.states.push((w, ~[]));
-        ~[Index(tail)]
+        ~[(Index(tail), Low)]
     }
 
     fn connect(&mut self, tails: &[Position]) {
         let head = self.states.len();
-        for &tail in tails.iter() {
-            match tail {
-                Initial => self.initial.push(head),
+        for &(pos, prec) in tails.iter() {
+            let targets: &mut ~[uint] = match pos {
+                Initial => &mut self.initial,
                 Index(index) => match self.states[index] {
-                    (_, ref mut targets) => targets.push(head)
+                    (_, ref mut targets) => targets
                 }
-            }
+            };
+            match prec {
+                Low => targets.push(head),
+                High => targets.insert(0, head)
+            };
         }
     }
 
@@ -80,7 +94,27 @@ fn compile_expr(b: &mut Builder, prev: &[Position], e: &Expr) -> ~[Position] {
             }
             tails
         },
-        //parse::Repeat(ref inner, min, max, greedy) => {},
+        parse::Repeat(ref inner, min, Some(max), greedy) => {
+            let mut tails = ~[];
+            let mut last = prev.to_owned();
+            for i in range(0, max) {
+                if i >= min {
+                    tails.push_all(last);
+                }
+                last = compile_expr(b, last, *inner);
+            }
+
+            if greedy == NonGreedy {
+                // Give all early-exit transitions higher priority
+                for tail in tails.mut_iter() {
+                    let &(pos, _) = tail;
+                    replace(tail, (pos, High));
+                }
+            }
+
+            tails.push_all(last);
+            tails
+        },
         parse::Repeat(..) => fail!("repeats not implemented yet"),
         parse::Capture(..) => fail!("captures not implemented yet")
     }
